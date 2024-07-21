a41c0c4a329211e30da89639157b40a3a23b1205
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2024-07-21 20:11:31 +0200

    [GH-5] feat(persistence): add metadata to mongo repository

--- src/Contexts/pecera/Auth/domain/UserRepository.ts
+++ src/Contexts/pecera/Auth/domain/UserRepository.ts
@@ -1,11 +1,12 @@
 import { Nullable } from '../../../shared/domain/Nullable';
 import { User } from './User';
+import { Username } from './Username';
 import { UserPatch } from './UserPatch';
 
 export interface UserRepository {
   save(user: User): Promise<void>;
 
-  update(user: UserPatch): Promise<void>;
+  update(user: UserPatch, username: Username): Promise<void>;
 
   search(email: string): Promise<Nullable<User>>;
 }
